033b7326603b56e6c520566b423c57a91792b055
Peace <onyeepeaz@gmail.com>
2020-07-17 14:49:47 +0100

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Navigation from './Navigation';
+import Header from './Header';
 import * as serviceWorker from './serviceWorker';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation logoTitle="React Project" />
+        <Header title="Stylish Portfolio" button="Find Out More" />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
